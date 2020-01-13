14b1a5d3186263e592e507b6d46003cd90ce5e24
Al3dekev <quentin1998@me.com>
2020-01-14 00:04:35 +0100

    Code: First components implementations

---
+++ src/app/components/decryption-page/decryption-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decryption-page',
+  templateUrl: './decryption-page.component.html',
+  styleUrls: ['./decryption-page.component.css']
+})
+export class DecryptionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/encryption-page/encryption-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encryption-page',
+  templateUrl: './encryption-page.component.html',
+  styleUrls: ['./encryption-page.component.css']
+})
+export class EncryptionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
