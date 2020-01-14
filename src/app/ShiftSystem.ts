b4dcd7dac0e09c1955c9f2646becabce4196f027
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2020-01-15 00:05:21 +0100

    Code: Beginning to see more about the encrypting system.

--- src/app/ShiftSystem.ts
+++ src/app/ShiftSystem.ts
@@ -8,19 +8,80 @@ export class ShiftSystem{
   }
 
   backToMainMenu(){
-    this.cs.pageArea = 0
+    this.cs.pageArea = 0;
 
   }
 
 
   EncryptingData(text:string, decal:number){
-    //process
-    //return output text
+
+    let strEncoded = '';
+    let letter = '';
+    let codeASCII = 0;
+    if(decal < 0){
+      decal = decal + 26;
+    }
+
+    for(i = 0; i < text.length; i++){
+
+      codeASCII = text[i].charCodeAt(0)
+
+      //Majuscule
+      if ((codeASCII >= 65) && (codeASCII <= 90)){
+        if ((codeASCII + decal) > 90){
+          letter = String.fromCharCode((65+((codeASCII+decal)-90))-1);
+        }else{
+          letter = String.fromCharCode(codeASCII+decal);
+        }
+      } else if((codeASCII >= 97) && (codeASCII <= 122)) {
+        if ((codeASCII + decal) > 122){
+          letter = String.fromCharCode((97+((codeASCII+decal)-122))-1);
+        }else{
+          letter = String.fromCharCode(codeASCII+decal);
+        }
+      }else{
+        letter = text[i]
+      }
+
+
+      strEncoded += letter;
+
+    }
+    return strEncoded
   }
 
   DecryptingData(text:string, decal:number){
-    //process
-    //return output text
+    let strEncoded = '';
+    let letter = '';
+    let codeASCII = 0;
+
+    if(decal < 0){
+      decal + 26
+    }
+
+    for(i=0;i<text.length;i++){
+
+      codeASCII = text[i].charCodeAt(0)
+
+      if ((codeASCII >= 65) && (codeASCII <= 90)){
+        if ((codeASCII - decal) < 65){
+          letter = String.fromCharCode(90-(65-(codeASCII-decal))+1);
+        }else{
+          letter = String.fromCharCode(codeASCII-decal);
+        }
+
+      } else if ((codeASCII >= 97) && (codeASCII <= 122)) {
+        if ((codeASCII - decal) < 97){
+          letter = String.fromCharCode(122-(97-(codeASCII-decal))+1);
+        }else{
+          letter = String.fromCharCode(codeASCII-decal);
+        }
+      }else{
+        letter = text[i]
+      }
+      strDecoded += letter;
+    }
+    return strDecoded
   }
 
 
