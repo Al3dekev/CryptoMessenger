40516fe3e1592fe43d9805118b34e572a63b0dc2
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2020-01-20 10:26:52 +0100

    Code: New LOGO added.

--- launch.js
+++ launch.js
@@ -7,6 +7,7 @@ function createWindow () {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + '/icon.png',
     webPreferences: {
       nodeIntegration: true
     },
